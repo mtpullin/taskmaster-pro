c177407eca9e4d724b3cfaf72fe7a1a98ba328fa
mtpullin <mtpullin@gmail.com>
2021-05-20 15:13:38 -0400

    final product

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,15 +55,23 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
+    $(this).addClass("dropover")
+    $(".bottom-trash").addClass("bottom-trash-drag");
     console.log(ui);
   },
   deactivate: function(event, ui) {
+    $(this).removeClass("dropover")
+    $(".bottom-trash").removeClass("bottom-trash-drag")
     console.log(ui);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active")
     console.log(event);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active")
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log(event);
   },
   update: function() {
@@ -131,7 +139,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -272,8 +280,12 @@ var auditTask = function(taskEl) {
     $(taskEl).addClass("list-group-item-warning");
   }
   //this should print out an object for the value of the date variable, but at 5:00pm of that date
-  console.log(time);
+  console.log(taskEl);
 };
-
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  })
+},1800000);
 // load tasks for the first time
 loadTasks();
